e950aeec639ff851c3ed04d1b31b3380256fc864
shirel taieb <shitaieb@g.jct.ac.il>
2022-10-30 22:37:26 +0200

    initial code+welcome func

---
+++ Stage0/Program9608.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9608();
+            Welcome0371();
+            Console.ReadKey();
+        }
+        static partial void Welcome0371();
+          private static void Welcome9608()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.Write("{0}, welcome  to my first console application", userName);
+        }
+    }
+
+}
