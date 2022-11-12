acee7a787bcf21ac9c9a0f4891d380ae7ecee9a7
TamarHayat <tahayat@g.jct.ac.il>
2022-11-13 00:18:45 +0200

    ועכשיו?

--- DalFacade/DO/Order.cs
+++ DalFacade/DO/Order.cs
@@ -4,14 +4,14 @@ namespace DO;
 public struct Order
 {
     public int ID { get; set; }
-    public string CustomerName { get; set; }
-    public string Email { get; set; }
-    public string CostomerAdress { get; set; }
-    public DateTime OrderDate { get; set; }
-    public OrderStatus Status { get; set; }
-    public DateTime PaymentDate { get; set; }
-    public DateTime ShipDate { get; set; }
-    public DateTime DeliveryDate { get; set; }
+    public string? CustomerName { get; set; }
+    public string? CostomerEmail { get; set; }
+    public string? CostomerAdress { get; set; }
+    public DateTime? OrderDate { get; set; }
+    public OrderStatus? Status { get; set; }
+    public DateTime? PaymentDate { get; set; }
+    public DateTime? ShipDate { get; set; }
+    public DateTime? DeliveryDate { get; set; }
     public OrderItem Item { get; set; }
     public double TotalPrice { get; set; }
 
---
+++ DalFacade/DalApi/ICrud.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalApi;
+public interface ICrud<T> where T : struct 
+{
+    int Add(T item);
+    T GetById(int id);
+    void Update(T item);
+    void Delete(int id);
+
+    //IEnumerable<T?> GetAll(Func<T?, bool>? filter = null);
+    IEnumerable<T> GetAll();
+}
