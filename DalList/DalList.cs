aaea75336fca2a952d009c1bffc3014b36ba1b46
TamarHayat <tahayat@g.jct.ac.il>
2022-11-20 20:33:47 +0200

    dallist

---
+++ DalList/DalList.cs
@@ -0,0 +1,12 @@
+﻿using DalApi;
+using System;
+
+
+namespace Dal;
+
+sealed internal class DalList : IDal
+{
+    public IOrder Order => new DalOrder();
+    public IProduct Product => new DalProduct();
+    public IOrderItem OrderItem => new DalOrderItem();
+}
