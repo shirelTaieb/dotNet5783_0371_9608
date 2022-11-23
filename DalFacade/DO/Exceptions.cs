7e53bd340a3f613f3641b08d094f7b53f6cf9aa0
TamarHayat <tahayat@g.jct.ac.il>
2022-11-23 13:21:20 +0200

    Stage1 Final commit

---
+++ DalFacade/DO/Exceptions.cs
@@ -0,0 +1,8 @@
+﻿public class NotExistException:Exception
+{
+    public override string Message => "missing item";
+}
+public class doubleException : Exception
+{
+    public override string Message => "already existing item";
+}
