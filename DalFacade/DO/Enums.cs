076d688861abdf02b651929881bef92b7021155a
TamarHayat <tahayat@g.jct.ac.il>
2022-11-29 21:24:43 +0200

    ybybybybybyb

--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -6,7 +6,5 @@ using System.Threading.Tasks;
 
 namespace DO
 {
-    internal class Enums
-    {
-    }
+  
 }
--- DalList/DalList.cs
+++ DalList/DalList.cs
@@ -7,8 +7,7 @@ namespace Dal;
 sealed internal class DalList : IDal
 {
     public static IDal Instance { get; } = new DalList();
-
-
+    private DalList() => new DalList();
     public IOrder Order => new DalOrder();
     public IProduct Product => new DalProduct();
     public IOrderItem OrderItem => new DalOrderItem();
