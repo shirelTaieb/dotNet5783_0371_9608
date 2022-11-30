dad1856903f592ce01470f37f59a3915e55bc7f6
shirel taieb <shitaieb@g.jct.ac.il>
2022-11-30 13:01:12 +0200

    צחיעכ

--- DalFacade/DalApi/DalConfig.cs
+++ DalFacade/DalApi/DalConfig.cs
@@ -9,22 +9,6 @@ using System.Xml.Linq;
 namespace DalApi;
 
 
-static class DalConfig
-{
-    internal static string? s_dalName;
-    internal static Dictionary<string, string> s_dalPackages;
-
-    static DalConfig()
-    {
-        XElement dalConfig = XElement.Load(@"xml\dal-config.xml")
-            ?? throw new DalConfigException("dal-config.xml file is not found");
-        s_dalName = dalConfig?.Element("dal")?.Value
-            ?? throw new DalConfigException("<dal> element is missing");
-        var packages = dalConfig?.Element("dal-packages")?.Elements()
-            ?? throw new DalConfigException("<dal-packages> element is missing");
-        s_dalPackages = packages.ToDictionary(p => "" + p.Name, p => p.Value);
-    }
-}
 
 [Serializable]
 public class DalConfigException : Exception
@@ -33,3 +17,49 @@ public class DalConfigException : Exception
     public DalConfigException(string msg, Exception ex) : base(msg, ex) { }
 }
 
+static class DalConfig
+{
+    internal class DalImplementation
+    {
+        internal string? _package;
+        internal string? _namespace;
+        internal string? _class;
+    }
+    internal static string? s_dalName;
+    internal static Dictionary<string, DalImplementation> s_dalPackages;
+    static DalConfig()
+    {
+        XElement dalConfig = XElement.Load(@"xml\dal-config.xml") ?? throw new DalConfigException("dal-config.xml file is not found");
+        s_dalName = dalConfig?.Element("dal")?.Value ?? throw new DalConfigException("<dal> element is missing");
+        var packages = dalConfig?.Element("dal-packages")?.Elements() ?? throw new DalConfigException("<dal-packages> element is missing");
+        s_dalPackages = (from item in packages
+                         let pkg = item.Value
+                         let ns = item.Attribute("namespace")?.Value ?? "Dal"
+                         let cls = item.Attribute("class")?.Value ?? pkg
+                         select new
+                         {
+                             item.Name,
+                             Value = new DalImplementation { _package = pkg, _namespace = ns, _class = cls }
+                         }
+        ).ToDictionary(p => "" + p.Name, p => p.Value);
+    }
+}
+
+
+// סתם כי אני מפחדת למחוק
+//static class DalConfig
+//{
+//    internal static string? s_dalName;
+//    internal static Dictionary<string, string> s_dalPackages;
+
+//    static DalConfig()
+//    {
+//        XElement dalConfig = XElement.Load(@"xml\dal-config.xml")
+//            ?? throw new DalConfigException("dal-config.xml file is not found");
+//        s_dalName = dalConfig?.Element("dal")?.Value
+//            ?? throw new DalConfigException("<dal> element is missing");
+//        var packages = dalConfig?.Element("dal-packages")?.Elements()
+//            ?? throw new DalConfigException("<dal-packages> element is missing");
+//        s_dalPackages = packages.ToDictionary(p => "" + p.Name, p => p.Value);
+//    }
+//}
