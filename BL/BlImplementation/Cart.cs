526e4b66768548af5e5f65ad9333b835d3604853
TamarHayat <tahayat@g.jct.ac.il>
2022-12-06 12:02:34 +0200

    productlist grafic

--- BL/BlImplementation/Cart.cs
+++ BL/BlImplementation/Cart.cs
@@ -14,13 +14,6 @@ namespace BlImplementation
         private IDal? Dal = DalApi.Factory.Get();
         public BO.Cart addProductToCart(BO.Cart? cart,int prID)
         {
-            DO.Product dproduct = new DO.Product();
-            dproduct= Dal.Product.GetById(prID)??throw new doseNotExistException();
-            BO.OrderItem orit = new BO.OrderItem();
-            orit.ProductID = dproduct.ID;
-            orit.Price = dproduct.Price;
-            if (cart == null)
-                cart.Items.Add(orit);
             if (cart.Items.Exists(or => or.ID == prID ))
             {
                 DO.Product? temp=new DO.Product();
@@ -30,6 +23,16 @@ namespace BlImplementation
                     temp.inStock--;
                     BO.OrderItem? oi = cart.Items.find(or => or.ID == prID); //מה יש לו
                 }
+                else throw new doseNotExistException(); //if there is no products in the stock
+            }
+            else
+            {
+                DO.Product dproduct = new DO.Product();
+                dproduct = Dal.Product.GetById(prID) ?? throw new doseNotExistException();
+                BO.OrderItem orit = new BO.OrderItem();
+                orit.ProductID = dproduct.ID;
+                orit.Price = dproduct.Price;
+                cart.Items.Add(orit);
             }
         }
         public BO.Cart updatPoductAmount(BO.Cart? cart)
