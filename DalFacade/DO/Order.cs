03127ce5aebe7da1b4ec7f3835a92a83e32d7890
TamarHayat <tahayat@g.jct.ac.il>
2022-12-07 09:30:31 +0200

    לא הספקתי לסיים עדיין את הפונקציה

--- DalFacade/DO/Order.cs
+++ DalFacade/DO/Order.cs
@@ -7,15 +7,15 @@ public struct Order
 {
  
     public int ID { get; set; }
-    public string? CostumerName { get; set; }
-    public string? CostumerEmail { get; set; }
-    public string? CostumerAdress { get; set; }
+    public string? CustomerName { get; set; }
+    public string? CustomerEmail { get; set; }
+    public string? CustomerAddress { get; set; }
     public DateTime? OrderDate { get; set; }
     public DateTime? ShipDate { get; set; }
     public DateTime? DeliveryDate { get; set; }
     public override string ToString() => $@"Product ID={ID}
-CostumerName- {CostumerName}
-Costumer Email- {CostumerEmail}
+CostumerName- {CustomerName}
+Costumer Email- {CustomerEmail}
 Order Date: {OrderDate}
 Ship Date: {ShipDate}
 Delivery Date: {DeliveryDate}
