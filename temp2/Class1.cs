25c30c3cbae25dc9ad8bdb8de24aa517fa9ae9d4
TamarHayat <tahayat@g.jct.ac.il>
2022-12-24 23:33:43 +0200

    add temp2

---
+++ temp2/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temp2
+{
+    public enum OrderStatus { confirm, sent, deliveried };
+    public class OrderForList
+    {
+        public int ID { get; set; }
+        public string CustomerName { get; set; }
+        public OrderStatus Status { get; set; }
+        public int AmountOfItems { get; set; }
+        public double TotalPrice { get; set; }
+  
+    }
+}
