99f9ef9e1c5aac1da29e0e9f9d0c54dfdc36422f
TamarHayat <tahayat@g.jct.ac.il>
2023-01-10 11:02:21 +0200

    dalxml

---
+++ DalXml/DalProduct.cs
@@ -0,0 +1,13 @@
+﻿using DalApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    internal class DalProduct:IProduct
+    {
+    }
+}
---
+++ DalXml/DalXml.cs
@@ -0,0 +1,18 @@
+﻿using DalApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    sealed internal class DalXml:IDal
+    {
+        public IProduct Product { get; } = new DalProduct();
+        public IOrder Order { get; } = new DalOrder();
+        public IOrderItem OrderItem { get; } = new DalOrderItem();
+
+    }
+}
