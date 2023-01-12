4bac4bcf4bd1c64dab04d23a46eb855dba60f711
shirel taieb <shitaieb@g.jct.ac.il>
2023-01-12 10:55:24 +0200

    change

--- DalXml/DalOrder.cs
+++ DalXml/DalOrder.cs
@@ -4,11 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DalApi;
+using System.Xml.Linq;
 
 namespace Dal
 {
     internal class DalOrder:IOrder
     {
+        const string s_orders = "orders";
+        public int Add(DO.Order order)
+        {
+            XElement ordersRootElem = XMLTools.LoadListFromXMLElement(s_orders);
 
+            if (XMLTools.LoadListFromXMLElement(s_orders)?.Elements()
+                .FirstOrDefault(st => st.ToIntNullable("ID") == order.ID) is not null)
+                // fix to: throw new DalMissingIdException(id);;
+                throw new Exception("id already exist");
+
+            ordersRootElem.Add(new XElement("Order", createStudentElement(order)));
+            XMLTools.SaveListToXMLElement(ordersRootElem, s_orders);
+
+            return order.ID; ;
+        }
     }
 }
