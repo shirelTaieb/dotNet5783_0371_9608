4cf0de0d1382fcde8238fce1a5c6ae3b1b814b31
shirel taieb <shitaieb@g.jct.ac.il>
2023-01-17 11:28:59 +0200

    grouping

--- PL/orders/orderListPage.xaml.cs
+++ PL/orders/orderListPage.xaml.cs
@@ -74,16 +74,16 @@ namespace PL.orders
                 MessageBox.Show(ex.Message, "", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
-      //  private void Status_DropDownOpened(object sender, EventArgs e)
-      //  {
-      //      IEnumerable<IGrouping<BO.OrderStatus?, BO.OrderForList>> groupings = GroupByStatus(bl!.Order!.getOrderList()!);
-      //      groupings = groupings.OrderBy(p => p.Key);
-      //      foreach (var group in groupings)
-      //          StatusComboBox.Items.Add(group.Key);
-      //  }
-      //  IEnumerable<IGrouping<BO.OrderStatus?, BO.OrderForList>> GroupByStatus(IEnumerable<BO.OrderForList> listToGroup)
-      //=> (from order in listToGroup
-      //    group order by order.Status into orderinfo
-      //    select orderinfo).ToList();
+        private void Status_ComboBox(object sender, EventArgs e)
+        {
+            IEnumerable<IGrouping<BO.OrderStatus?, BO.OrderForList>> groupings = GroupByStatus(bl!.Order!.getOrderList()!);
+            groupings = groupings.OrderBy(p => p.Key);
+            foreach (var group in groupings)
+                statusSelector.Items.Add(group.Key);
+        }
+        IEnumerable<IGrouping<BO.OrderStatus?, BO.OrderForList>> GroupByStatus(IEnumerable<BO.OrderForList> listToGroup)
+      => (from order in listToGroup
+          group order by order.Status into orderinfo
+          select orderinfo).ToList();
     }
 }
