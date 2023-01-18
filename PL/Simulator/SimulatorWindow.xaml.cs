ba742c4e3c2c5f760ec1be6fb680b77f493faee8
shirel taieb <shitaieb@g.jct.ac.il>
2023-01-18 15:35:14 +0200

    byeeeee

--- PL/Simulator/SimulatorWindow.xaml.cs
+++ PL/Simulator/SimulatorWindow.xaml.cs
@@ -1,50 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.ComponentModel;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Shapes;
+//using System.ComponentModel;
 
 
-namespace PL.Simulator
-{
-   // BackgroundWorker worker;
-    /// <summary>
-    /// Interaction logic for SimulatorWindow.xaml
-    /// </summary>
-    public partial class SimulatorWindow : Window
-    {
-        private BLApi.IBl? bl = BLApi.Factory.Get();
-        public SimulatorWindow()
-        {
-            InitializeComponent();
-            //worker = new BackgroundWorker();
-            //worker.DoWork += Worker_DoWork;
-            //worker.ProgressChanged += Worker_ProgressChanged;
-            //worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+//namespace PL.Simulator
+//{
 
-            //worker.WorkerReportsProgress = true;
-            //worker.RunWorkerAsync("argument");
+//    /// <summary>
+//    /// Interaction logic for SimulatorWindow.xaml
+//    /// </summary>
+//    public partial class SimulatorWindow : Window
+//    {
+//        BackgroundWorker Tali;
+//        private BLApi.IBl? bl = BLApi.Factory.Get();
+//        public SimulatorWindow()
+//        {
+//            InitializeComponent();
+//            Tali = new BackgroundWorker();
+//            Tali.DoWork += Tali_DoWork!;
+//            Tali.ProgressChanged += Tali_ProgressChanged!;
+//            Tali.RunWorkerCompleted += Tali_RunWorkerCompleted!;
+
+//            Tali.WorkerReportsProgress = true;
+//            Random rand=new Random();
+//            int argu =rand.Next(5,10);
+//            Tali.RunWorkerAsync(argu);
+
+//            orderSimulationList.DataContext =
+//                from or in bl!.Order!.getOrderList()
+//                select new PO.OrderForList()
+//                {
+//                    ID = or.ID,
+//                    CustomerName = or.CustomerName,
+//                    AmountOfItems = or.AmountOfItems,
+//                    Status = (BO.HebOrderStatus?)or.Status,
+//                    TotalPrice = or.TotalPrice
+//                };
+//        }
+//        private void Tali_DoWork(object sender, DoWorkEventArgs e)
+//        {
+//            for(int i)
+//            object obj = e.Argument;
+
+//            Tali.ReportProgress(1);
+
+//            e.Result = "result";
+
+//        }
+//        private void Tali_ProgressChanged(object sender, ProgressChangedEventArgs e)
+//        {
+//            int progress = e.ProgressPercentage;
+//        }
+//        private void Tali_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+//        {
+//            object result = e.Result;
+//        }
 
 
-            orderSimulationList.DataContext =
-                from or in bl!.Order!.getOrderList()
-                select new PO.OrderForList()
-                {
-                    ID = or.ID,
-                    CustomerName = or.CustomerName,
-                    AmountOfItems = or.AmountOfItems,
-                    Status = (BO.HebOrderStatus?)or.Status,
-                    TotalPrice = or.TotalPrice
-                };
-        }
-    }
-}
+//    }
+//}
